8050a36024b35bec36c6e8d54965f768743df837
dylan0000 <58610746+dylan0000@users.noreply.github.com>
2020-02-27 20:20:47 -0800

    please save

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 namespace SpriteKind {
     export const cursor = SpriteKind.create()
     export const money = SpriteKind.create()
+    export const shop = SpriteKind.create()
 }
 controller.up.onEvent(ControllerButtonEvent.Released, function () {
     if (stilltitlescreen == true) {
@@ -39,121 +40,46 @@ scene.onHitTile(SpriteKind.Player, 9, function (sprite) {
             cashAmount = 0
         } else if (moneyinteraction == "withdraw") {
             cashAmount += BANKaccount
-            game.splash("Amount taken: " + cashAmount)
+            game.splash("Amount taken: " + BANKaccount)
             BANKaccount = 0
         } else if (moneyinteraction == "no") {
             game.splash("ok")
         }
     }
 })
-function Arrays () {
-    // rubber duck cost for 1 = $10
-    //
-    // damage = 2
-    //
-    // pellet cost for 1 = $16
-    //
-    // damage = 3.5
-    //
-    // bomb cost for 1 = $26
-    //
-    // damage = 8
-    //
-    // star cost for 1 = $22
-    //
-    //
-    // damage = 4
-    //
-    // coin cost for 1 = $33
-    //
-    //
-    // damage = 12
-    Available_weapon_sprites = [img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . 5 5 . . . . 
-. . . . . . . . . 5 5 5 5 . . . 
-. . . . . . . . . 5 f 5 4 4 . . 
-. . . . . . 5 5 5 5 5 5 . . . . 
-. . . . 5 5 5 5 5 5 5 . . . . . 
-. . . . . 5 5 5 5 5 5 . . . . . 
-. . . . . . 5 5 5 5 . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . . . 1 1 . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . 5 . . . . . . . . 
-. . . . . . . . b . . . . . . . 
-. . . . . . . . b . . . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . . f f f f f f . . . . . 
-. . . . f f f f f f f f . . . . 
-. . . . f f f f f f f f . . . . 
-. . . . f f f f f f f f . . . . 
-. . . . f f f f f f f f . . . . 
-. . . . . f f f f f f . . . . . 
-. . . . . . f f f f . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . 6 6 . . . . . . . . 
-. . . . . . 6 1 6 6 6 . . . . . 
-. . . . . . 6 1 1 1 6 . . . . . 
-. . . . . 6 1 1 1 6 . . . . . . 
-. . . . . 6 6 6 1 6 . . . . . . 
-. . . . . . . . 6 6 . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`, img`
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . f f f . . . . . . . 
-. . . . . f 5 5 5 f . . . . . . 
-. . . . f 5 5 5 5 5 f . . . . . 
-. . . . f 5 5 5 5 5 f . . . . . 
-. . . . f 5 5 5 5 5 f . . . . . 
-. . . . . f 5 5 5 f . . . . . . 
-. . . . . . f f f . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-. . . . . . . . . . . . . . . . 
-`]
+function Change_projectile_speed () {
+    if (weaponoption < 2 || weaponoption > 2) {
+        if (XVELOCITY == 100) {
+            XVELOCITY = 0
+            YVELOCITY = -100
+        } else if (YVELOCITY == -100) {
+            XVELOCITY = -100
+            YVELOCITY = 0
+        } else if (XVELOCITY == -100) {
+            XVELOCITY = 0
+            YVELOCITY = 100
+        } else if (YVELOCITY == 100) {
+            XVELOCITY = 100
+            YVELOCITY = 0
+        }
+    } else if (weaponoption == 2) {
+        XVELOCITY = XVELOCITY * -1
+    }
+}
+function Projectileshot () {
+    Arrays()
+    projectile = sprites.createProjectileFromSprite(Available_weapon_sprites[weaponoption], Person, XVELOCITY, YVELOCITY)
+    if (weaponoption == 2) {
+        projectile.ay = 320
+    }
+}
+function SET () {
+    BANKaccount = 0
+    cashAmount = 5
+    canshoot = false
+    levelnumber = 0
+    XVELOCITY = 100
+    YVELOCITY = 0
 }
 function LevelsList () {
     if (difficulty == 0) {
@@ -173,8 +99,8 @@ function LevelsList () {
 . . . . . b 1 b b b 1 b . . . . . b 1 b b 1 b . . . . . . . . . 
 . . . . . b b b b b b b . . . . . b 1 b b 1 b . . . . . . . . . 
 . . . . . b 1 b b b 1 b . . . . . b b b b b b . . . . . . . . f 
-. . 7 7 . b b b 5 b b b . . . . b b b 5 5 b b . . . . . . . . . 
-. . 7 7 . b b b 5 b b b 7 . . . 9 b b 5 5 b b . . . 7 . . 7 . . 
+. . 7 7 . b b b 5 b b b . . . . b b b 5 5 b b b . . . . . . . 5 
+. . 7 7 . b b b 5 b b b 7 . . . 9 b b 5 5 b b . . . 7 . . 7 . 5 
 f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f f 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . 
@@ -514,6 +440,11 @@ controller.down.onEvent(ControllerButtonEvent.Pressed, function () {
 `)
     }
 })
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (canshoot == true) {
+        Change_projectile_speed()
+    }
+})
 function Instructions () {
     game.showLongText("Collect money $$$ to advance. Use the money that you collect to upgrade your weapon. Money is also like score so collect the most to win", DialogLayout.Center)
     game.showLongText("Run into an ATM to check how much money you have, deposit money so you do not lose it on death, and withdraw money. Heres $5 to start", DialogLayout.Center)
@@ -558,6 +489,27 @@ controller.down.onEvent(ControllerButtonEvent.Released, function () {
 `)
     }
 })
+function makeShop () {
+    merchant = sprites.create(img`
+f f f f f f f f f f f f f f f f 
+f b b b f b f f b f b f f f b f 
+f b f b f b f f b f b f f f b f 
+f b b f f b f f b f f b f b f f 
+f b f b f b f f b f f f b f f f 
+f b b b f f b b f f f f b f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f b f b f b b b f b b f f b b b 
+f b f b f b f f f b f b f b f f 
+f b b b f b b f f b b f f b b f 
+f b f b f b f f f b f b f b f f 
+f b f b f b b b f b f b f b b b 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+f f f f f f f f f f f f f f f f 
+`, SpriteKind.shop)
+    tiles.placeOnTile(merchant, tiles.getTileLocation(23, 16))
+}
 function CreateCoins () {
     for (let value of scene.getTilesByType(7)) {
         COIN = sprites.create(img`
@@ -757,6 +709,45 @@ function CreateCoins () {
         scene.place(value, COIN)
     }
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.shop, function (sprite, otherSprite) {
+    BUY = game.askForString("want to buy something? ¨yes¨ or ¨no¨")
+    if (BUY == "yes") {
+        game.splash("Balance: " + cashAmount)
+        game.showLongText("Options available: rubber ducks for $30, pellets for $46, bombs for $69, stars for $50, and coins for $90. Note that these come in a pack of 25 ", DialogLayout.Full)
+        weaponoption = game.askForNumber("0.duck 1.pellet 2.bomb 3.star 4.coin")
+        if (weaponoption == 0 && cashAmount >= 30) {
+            cashAmount += -30
+            canshoot = true
+            damage = 2
+            game.splash("You received x25 ducks")
+        } else if (weaponoption == 1 && cashAmount >= 46) {
+            cashAmount += -46
+            canshoot = true
+            damage = 3.5
+            game.splash("You received x25 pellets")
+        } else if (weaponoption == 2 && cashAmount >= 69) {
+            cashAmount += -69
+            canshoot = true
+            XVELOCITY = 80
+            YVELOCITY = -100
+            damage = 8
+            game.splash("You received x25 bombs")
+        } else if (weaponoption == 3 && cashAmount >= 50) {
+            cashAmount += -50
+            canshoot = true
+            damage = 6
+            game.splash("You received x25 stars")
+        } else if (weaponoption == 4 && cashAmount >= 90) {
+            cashAmount += -90
+            canshoot = true
+            damage = 12
+            game.splash("You received x25 coins")
+        } else {
+            game.splash("You did not pick 0-4", "Or you are poor")
+        }
+        Person.x += 5
+    }
+})
 function Player2 () {
     info.setLife(4)
     Person = sprites.create(img`
@@ -780,6 +771,119 @@ function Player2 () {
     Person.ay = 350
     scene.cameraFollowSprite(Person)
     controller.moveSprite(Person, 100, 0)
+}
+scene.onHitTile(SpriteKind.Player, 5, function (sprite) {
+    levelnumber += 1
+    scene.setTileMap(LevelList[levelnumber])
+})
+function Arrays () {
+    // rubber duck cost for 1 = $10
+    //
+    // damage = 2
+    //
+    // pellet cost for 1 = $16
+    //
+    // damage = 3.5
+    //
+    // bomb cost for 1 = $26
+    //
+    // damage = 8
+    //
+    // star cost for 1 = $22
+    //
+    //
+    // damage = 4
+    //
+    // coin cost for 1 = $33
+    //
+    //
+    // damage = 12
+    Available_weapon_sprites = [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . 5 5 . . . . 
+. . . . . . . . . 5 5 5 5 . . . 
+. . . . . . . . . 5 f 5 4 4 . . 
+. . . . . . 5 5 5 5 5 5 . . . . 
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . . . . 5 5 5 5 5 5 . . . . . 
+. . . . . . 5 5 5 5 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . . 1 1 . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . 5 . . . . . . . . 
+. . . . . . . . b . . . . . . . 
+. . . . . . . . b . . . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . f f f f f f f f . . . . 
+. . . . . f f f f f f . . . . . 
+. . . . . . f f f f . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . 6 6 . . . . . . . . 
+. . . . . . 6 1 6 6 6 . . . . . 
+. . . . . . 6 1 1 1 6 . . . . . 
+. . . . . 6 1 1 1 6 . . . . . . 
+. . . . . 6 6 6 1 6 . . . . . . 
+. . . . . . . . 6 6 . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . f 5 5 5 f . . . . . . 
+. . . . f 5 5 5 5 5 f . . . . . 
+. . . . f 5 5 5 5 5 f . . . . . 
+. . . . f 5 5 5 5 5 f . . . . . 
+. . . . . f 5 5 5 f . . . . . . 
+. . . . . . f f f . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`]
 }
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     if (stilltitlescreen == true) {
@@ -913,7 +1017,18 @@ b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b b 
 `)
             tiles.placeOnTile(Person, tiles.getTileLocation(0, 16))
             CreateCoins()
+            makeShop()
         }
+    } else {
+        if (canshoot == true) {
+            Projectileshot()
+        }
+    }
+})
+scene.onHitWall(SpriteKind.Projectile, function (sprite) {
+    if (weaponoption == 2) {
+        scene.cameraShake(3, 200)
+        sprite.startEffect(effects.fire)
     }
 })
 controller.right.onEvent(ControllerButtonEvent.Pressed, function () {
@@ -1119,25 +1234,31 @@ function DoubleJump () {
         }
     }
 }
+let damage = 0
+let BUY = ""
 let COIN: Sprite = null
+let merchant: Sprite = null
 let Myheight = 0
 let doubleJump = false
 let canjump = false
 let LevelList: Image[] = []
+let levelnumber = 0
+let canshoot = false
 let Available_weapon_sprites: Image[] = []
+let projectile: Sprite = null
+let YVELOCITY = 0
+let XVELOCITY = 0
+let weaponoption = 0
+let BANKaccount = 0
 let moneyinteraction = ""
+let cashAmount = 0
 let difficulty = 0
 let Person: Sprite = null
 let mouse: Sprite = null
 let stilltitlescreen = false
-let levelnumber = 0
-let cashAmount = 0
-let BANKaccount = 0
 Instructions()
-BANKaccount = 0
-cashAmount = 5
-levelnumber = 0
 Title_Screen()
+SET()
 game.onUpdate(function () {
     if (stilltitlescreen == true) {
         if (mouse.y == 70) {
